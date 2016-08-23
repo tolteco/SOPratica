aa16dec3274caf80ff4f70296c59f4252938c348
Maycon <mscrat@hotmail.com>
2016-08-22 22:48:15 -0300

    BuddySystem
    Inicio; avancos mais significativos em leitura de dados (padrao de
    escrita a discutir) e impressao de relatorios de desempenho

---
+++ BuddySystem/src/controle/No.java
@@ -0,0 +1,17 @@
+/*
+ * No da arvore
+ */
+package controle;
+
+import javax.xml.soap.Node;
+
+/**
+ * 
+ * @author Maycon
+ */
+public class No {
+    public Node A;
+    public boolean ocupado;
+    public byte nivel;
+        
+}

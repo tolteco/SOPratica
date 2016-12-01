2bed0e299bc6831fecc063fc062394ace1e1e5de
AndersonBottega <andersonbottegas@hotmail.com>
2016-11-30 23:00:36 -0200

    Imprimir tabela
    nada testado

--- Trabalho 3 - FileSystem/CumuloSA.cpp
+++ Trabalho 3 - FileSystem/CumuloSA.cpp
@@ -8,6 +8,23 @@
 char comando[100], dir[102], com[5], com2[100];
 int exit = 0;
 
+void tabela(){//Esta fucao deixa a tabela de informacoes bonita
+  if (tam>=0 &&tam<10){
+      printf("%s %d         .     %d %s", data, hora, tam, nome); //nao sera impresso assim
+  }else if(tam>9 && tam<100){
+      printf("%s %d         .    %d %s", data, hora, tam, nome);
+  }else if(tam>99 && tam<1000){
+      printf("%s %d         .   %d %s", data, hora, tam, nome);
+  }else if(tam>999 && tam<10000){
+      printf("%s %d         .  %d %s", data, hora, tam, nome);
+  }else if(tam>9999 && tam<100000){
+      printf("%s %d         . %d %s", data, hora, tam, nome);
+  }else if(tam>99999 && tam<1000000){
+      printf("%s %d         .%d %s", data, hora, tam, nome);
+  }
+  return 0;
+}
+
 int main(int argc, char *op[]){
   if (argc == 2){ //Verifica se a quantidade de argumentos e valida
     if ((strcmp(op[1], "-h") == 0) || (strcmp(op[1], "/h") == 0) || (strcmp(op[1], "/help") == 0) || (strcmp(op[1], "-help") == 0)){ //Opcao Help
@@ -38,10 +55,30 @@ int main(int argc, char *op[]){
           printf("  ..           Ir a pasta pai");
         }
       }
-      if (strcmp(com, "exit") == 0 || strcmp(com, "EXIT") == 0){ //Opcao de saida
+      if else(strcmp(com, "exit") == 0 || strcmp(com, "EXIT") == 0){ //Opcao de saida
         exit = 1;
       }
     }
+    else if (strcmp(com, "cd") == 0 || strcmp(com, "CD") == 0){
+        com2 = strtok(NULL, " ");
+
+        while(strcmp(com2, NOME.nome) != 0){
+          fread(&NOME, 1, sizeof(NOME), d);//"d" e o ponteiro para o arquivo
+        }
+        if (NOME.tipo==0){
+          fseek//ver com o juliano
+        }
+      }
+      else if(strcmp(com, "dir") == 0 || strcmp(com, "DIR") == 0){
+        while(strcmp(com2, "") != 0){
+          fread(&NOME, 1, sizeof(NOME), d);//"d" e o ponteiro para o arquivo, PRECISO DO FSEEK AQUI E PRECISO SABER TBEM AS VARIAVEIS Q VIRAM DE VC PRA EU COLOCAR AQUI, MAS FAREI AMANHA ISSO
+          if(NOME.tipo==0){
+            printf("%s %d   <DIR>       %s", data, hora, nome);
+          }else{
+            tabela();
+          }
+        }
+      }
   } else {
     printf("Erro, quantidade de argumentos invalido.\n");
 		return -1;

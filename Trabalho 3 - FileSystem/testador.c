4767925dcb4b035d2a813aa428b934c6bcd85216
JulianoFelipe <juliano_lpj@hotmail.com>
2016-11-30 22:05:26 -0200

    Sum stuff
    upload por segurança. Muitas funções não testadas. Explosões estilo
    Michael Bay.

--- Trabalho 3 - FileSystem/testador.c
+++ Trabalho 3 - FileSystem/testador.c
@@ -1,13 +1,41 @@
 #include <stdio.h>
 #include <stdlib.h>
 #include "Macros.h"
+#include <string.h>
+#include "FileData.h"
+#include "InitBlock.h"
 
-int prototype (int param);
+
+void delimit(){
+    char *token;
+    char line[] = "AAAAA.txt";
+    char *search = ".";
+
+
+    // Token will point to "SEVERAL".
+    token = strtok(line, search);
+    printf("%s,", token);
+
+    // Token will point to "WORDS".
+    token = strtok(NULL, search);
+    printf("\n%s,", token);
+}
 
 int main (){
+    printf("LKJ\n\n");
+    char f[]= "AAABCDDD.txtaaaaaaaaa";
+printf("LKJ\n\n");
+    stringChopBegin(f, 3);
+    printf("%s, ", f);
+
+    /*FileAttribs* file = newFileAttribs();
+    strcpy(file->nome_arquivo, "0000000000000");
+    int i = isFileEmpty(file);
+
+    printf("%d\n\n\n", i);
+
+    tempoAtual();*/
 
     return 0; //Error Level
 }
 
-int prototype (int param){
-}
